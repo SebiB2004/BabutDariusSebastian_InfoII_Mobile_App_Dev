42ef21c5a5da198d2e2515c26cd282ecfce955f0
SebiB2004 <dariusbabuteu@gmail.com>
2025-03-09 20:53:37 +0200

    Ex1

---
+++ lab7-8/Ex1/exercitiul1/app/src/main/java/com/example/intents/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.intents
+import android.content.Intent
+import com.example.intents.ui.theme.IntentsAndIntentFiltersTheme
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setContent{
+
+            Column(
+                modifier = Modifier.fillMaxSize().padding(top = 100.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                       Intent(applicationContext, SecondActivity::class.java).also {
+                    startActivity(it)
+                }
+
+                }) {
+                    Text(text = "Acceseaza primul intent!")
+                }
+
+
+                Button(onClick = {
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        type = "text/plain"
+                        putExtra(Intent.EXTRA_EMAIL, arrayOf("dariusbabuteu@gmail.com"));
+                        putExtra(Intent.EXTRA_SUBJECT, "Test email");
+                        putExtra(Intent.EXTRA_TEXT, "Continutul emailului");
+                    }
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent)
+                    }
+                }) {
+                    Text("Acceseaza al doilea intent!")
+                }
+
+                Button(onClick = {
+                    Intent(Intent.ACTION_MAIN).also {
+                        it.`package` = "com.google.android.youtube"
+                        startActivity(it)
+                    }
+                })
+                    {
+                    Text("Acceseaza al treilea intent")
+                }
+
+            }
+
+            }
+        }
+    }
